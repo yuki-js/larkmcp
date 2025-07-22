b3f8bb819a4e91864a0164bba5dc1427360464ea
yuki-js <me@aoki.app>
2025-07-22 14:23:20 +0900

    fix path issue in windows

--- server/tools/fetchLarkDocIndex.js
+++ server/tools/fetchLarkDocIndex.js
@@ -1,8 +1,11 @@
 import fs from "fs/promises";
 import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const csvPath = path.join(
-  path.dirname(import.meta.url.replace("file://", "")),
+  __dirname,
   "..",
   "..",
   "data",
